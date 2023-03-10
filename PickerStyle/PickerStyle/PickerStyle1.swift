f2425f10edc75504acc86e91224c08f087a4c4c9
Elaidzha1940 <eli080293@gmail.com>
2023-03-10 20:40:53 +0700

    Picker Stylet1

--- PickerStyle/PickerStyle/PickerStyle1.swift
+++ PickerStyle/PickerStyle/PickerStyle1.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct PickerStyle1: View {
+    
+    @State private var selection: String = "1"
+    @State private var showPicker = false
+    var options = ["1 ч", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" ]
+    var totalWidth = UIScreen.main.bounds.width - 80
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
