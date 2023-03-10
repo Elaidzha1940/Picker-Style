6cd601c5dea3262117371a58efd20540237cc951
Elaidzha1940 <eli080293@gmail.com>
2023-03-10 19:54:43 +0700

    Picker Stylet

---
+++ PickerStyle/PickerStyle/PickerStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerStyleApp.swift
+//  PickerStyle
+//
+//  Created by Elaidzha Shchukin on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct PickerStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
